fd53e060be9f3dbac3df76dba2895c678247cce4
Nick.Wu <nick.wu@infortrend.com>
2023-07-24 17:47:53 +0800

    -

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import EquipmentForm from './EquipmentForm';
 import './style.css';
+import Sidebar from './sidebar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <Sidebar />
   <div className="app">
     <h1 className="title">裝備卷軸計算機</h1>
     <div className="container">
